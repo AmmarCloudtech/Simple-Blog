aa3f2d315354761668c6103bc774d1d4700efcd0
Ammar <ammar.cloudtech@gmail.com>
2017-06-20 16:21:52 -0400

    74 - Nesting Of Routes

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  // we removed <APP /> from here and replaced it with react-Router's instance because \
+  // will render app component somewhere else and we want react-router to be incharge of whats gonna be rendered
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.querySelector('.container'));
