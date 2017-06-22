6cb30a7bb6d9a8a8f106a9ce31ad74e11a8bcf71
Ammar <ammar.cloudtech@gmail.com>
2017-06-22 15:50:10 -0400

    85 Froms

--- ReduxSimpleStarter/src/components/posts_new.js
+++ ReduxSimpleStarter/src/components/posts_new.js
@@ -3,22 +3,26 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const { fields: {title, categories, content }, handleSubmit } = this.props; // same as const handleSubmit = this.props.handleSubmit;
+                                                                               // Just in the exact same way { connect } helper injects props into our component
+                                                                               // { reduxForm } does the exact same thing, so { reduxForm } is injecting some helpers for us
+                                                                               // onto this.props inside of the component below
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
         </div>
+        <input type="text" className="form-control" {...title} />
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
